68daaf0c7b3c985a50ba7eea824da6c9f3970260
Kilian Friedrich <30508429+WolvLey@users.noreply.github.com>
2020-04-11 22:47:09 +0200

    implement router link

--- src/common/components/Table/index.tsx
+++ src/common/components/Table/index.tsx
@@ -9,38 +9,44 @@ import Paper from '@material-ui/core/Paper';
 import Button from 'common/components/Button';
 import { IoIosArrowForward } from 'react-icons/io';
 
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
 import './style.scss';
 import { ITableItem } from 'common/interfaces/ITableItem';
 
 export default function SimpleTable({ items }: { items: ITableItem }) {
 	return (
-		<TableContainer component={Paper}>
-			<Table className='table' aria-label='simple table'>
-				<TableHead className='table-head'>
-					<TableRow>
-						{items.titles.map((row: String, index: number) => (
-							<TableCell key={index}>
-								<span className='cell'>{row}</span>
-							</TableCell>
-						))}
-					</TableRow>
-				</TableHead>
-				<TableBody className='table-body'>
-					{items.data.map((row: any, index: number) => (
-						<TableRow key={index}>
-							<TableCell component='th' scope='row'>
-								{row.serverName}
-							</TableCell>
-							<TableCell>{row.game}</TableCell>
-							<TableCell>
-								<Button color={'primary'}>
-									<IoIosArrowForward />
-								</Button>
-							</TableCell>
+		<Router>
+			<TableContainer component={Paper}>
+				<Table className='table' aria-label='simple table'>
+					<TableHead className='table-head'>
+						<TableRow>
+							{items.titles.map((row: String, index: number) => (
+								<TableCell key={index}>
+									<span className='cell'>{row}</span>
+								</TableCell>
+							))}
 						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+					</TableHead>
+					<TableBody className='table-body'>
+						{items.data.map((row: any, index: number) => (
+							<TableRow key={index}>
+								<TableCell component='th' scope='row'>
+									{row.serverName}
+								</TableCell>
+								<TableCell>{row.game}</TableCell>
+								<TableCell>
+									<Button color={'primary'}>
+										<Link to={`/gameservers/${row.game}`}>
+											<IoIosArrowForward />
+										</Link>
+									</Button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</Router>
 	);
 }
